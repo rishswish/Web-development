01e7a13861fc9df8d0dc0a93c933e98bcafd7388
Rishabh <rishabhpatil9179@gmail.com>
2022-09-14 19:57:55 +0530

    did changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const lodash = require("lodash");
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/blogDB')
+mongoose.connect('mongodb+srv://admin-rishabh:Bhaskar9@cluster0.tcdkqs1.mongodb.net/?retryWrites=true&w=majority/blogDB')
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -83,6 +83,6 @@ app.get("/posts/:postID", function (req, res) {
   })
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
